ffa6e2823666edcac63f2c96ed335efd82771c3a
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-05-30 11:15:01 -0400

    Autocommit por GitHub.sh: alterações: add 'bspwm/.config/bspwm/bspwmrc' add 'home/bin/bg.sh'

--- home/bin/bg.sh
+++ home/bin/bg.sh
@@ -8,7 +8,8 @@ _screen() {
     xwinwrap -ov -ni -g '1920x1080+0+0' -- mpv --fullscreen \
         --no-stop-screensaver \
         --vo=vdpau --hwdec=vdpau \
-        --loop-file --no-audio --no-osc --no-osd-bar -wid WID --no-input-default-bindings \
+        --loop-file --no-audio --no-osc --no-osd-bar -wid WID \
+		--no-input-default-bindings --autofit=100%x100% --panscan=1.0 \
         "$1" &
     PIDs+=($!)
 }
