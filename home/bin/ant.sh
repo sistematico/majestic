8d3ee0a15ce13dc1790fd4978c608d8db77223cb
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-06-08 20:15:02 -0400

    Autocommit por GitHub.sh: alterações: add 'home/bin/ant.sh'

--- home/bin/ant.sh
+++ home/bin/ant.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+gsettings set org.xfce.mousepad.preferences.view color-scheme 'nord'
 gsettings set org.gnome.desktop.interface gtk-theme "Ant-Dracula"
 gsettings set org.gnome.desktop.wm.preferences theme "Ant-Dracula"
 gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
