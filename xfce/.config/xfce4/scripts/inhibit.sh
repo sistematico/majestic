a6b38a368d2a58ec9c259e8c8af41369448c8ad7
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-06-13 17:20:01 -0400

    Autocommit por GitHub.sh: alterações: remove 'home/.local/bin/inhibit' add 'xfce/.config/xfce4/scripts/inhibit.sh'

--- xfce/.config/xfce4/scripts/inhibit.sh
+++ xfce/.config/xfce4/scripts/inhibit.sh
@@ -3,7 +3,7 @@
 STATUS=$(pacmd list-sink-inputs | awk -F': ' '/state/{print $2}')
 
 if [ "$STATUS" == "RUNNING" ]; then
-	xfce4-screensaver-command -p
+	DISPLAY=:0 /usr/bin/xfce4-screensaver-command -p
 	echo "<img>$HOME/.local/share/icons/elementary/user-available.svg</img>"
 else
 	echo "<img>$HOME/.local/share/icons/elementary/user-busy.svg</img>"
