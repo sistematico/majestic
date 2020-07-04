2523edcad28caa6f0212cada20c9afc7afeae76d
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-04 07:59:01 -0400

    Autocommit por GitHub.sh: alterações: add 'startpage/.config/startpage/js/data.js'

--- startpage/.config/startpage/js/data.js
+++ startpage/.config/startpage/js/data.js
@@ -7,12 +7,15 @@
 //                 + currentdate.getSeconds();
 
 var currentdate = new Date(); 
-var datetime = "Last Sync: " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + ('0' + currentdate.getHours()).slice(-2) + ":"  
-                + ('0' + currentdate.getMinutes()).slice(-2) + ":" 
-                + ('0' + currentdate.getSeconds()).slice(-2);
 
-//document.getElementById("data").innerHTML = datetime;
-document.querySelector('.data').innerHTML = datetime;
+let interval = setInterval(function() {
+    var datetime = currentdate.getDate() + "/"
+    + (currentdate.getMonth()+1)  + "/" 
+    + currentdate.getFullYear() + " @ "  
+    + ('0' + currentdate.getHours()).slice(-2) + ":"  
+    + ('0' + currentdate.getMinutes()).slice(-2) + ":" 
+    + ('0' + currentdate.getSeconds()).slice(-2);
+
+
+    document.querySelector('.data').innerHTML = datetime;
+},1000);
