97cfd8c7b5581705676ee020d195c6dc743499a0
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-04 07:58:01 -0400

    Autocommit por GitHub.sh: alterações: add 'startpage/.config/startpage/js/data.js'

--- startpage/.config/startpage/js/data.js
+++ startpage/.config/startpage/js/data.js
@@ -1,19 +1,18 @@
-// var today = new Date();
-// var dd = String(today.getDate()).padStart(2, '0');
-// var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-// var yyyy = today.getFullYear();
-
-// today = mm + '/' + dd + '/' + yyyy;
-// document.write(today);
-
+// var currentdate = new Date(); 
+// var datetime = "Last Sync: " + currentdate.getDate() + "/"
+//                 + (currentdate.getMonth()+1)  + "/" 
+//                 + currentdate.getFullYear() + " @ "  
+//                 + currentdate.getHours() + ":"  
+//                 + currentdate.getMinutes() + ":" 
+//                 + currentdate.getSeconds();
 
 var currentdate = new Date(); 
 var datetime = "Last Sync: " + currentdate.getDate() + "/"
                 + (currentdate.getMonth()+1)  + "/" 
                 + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
+                + ('0' + currentdate.getHours()).slice(-2) + ":"  
+                + ('0' + currentdate.getMinutes()).slice(-2) + ":" 
+                + ('0' + currentdate.getSeconds()).slice(-2);
 
 //document.getElementById("data").innerHTML = datetime;
 document.querySelector('.data').innerHTML = datetime;
