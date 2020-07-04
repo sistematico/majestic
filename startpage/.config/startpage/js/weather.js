681077ac607a8fe5f6a5b3f3303b5982b6fe4ecc
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-04 08:10:01 -0400

    Autocommit por GitHub.sh: alterações: add 'startpage/.config/startpage/js/weather.js'

---
+++ startpage/.config/startpage/js/weather.js
@@ -0,0 +1,18 @@
+// var currentdate = new Date(); 
+// var datetime = "Last Sync: " + currentdate.getDate() + "/"
+//                 + (currentdate.getMonth()+1)  + "/" 
+//                 + currentdate.getFullYear() + " @ "  
+//                 + currentdate.getHours() + ":"  
+//                 + currentdate.getMinutes() + ":" 
+//                 + currentdate.getSeconds();
+
+let interval = setInterval(function() {
+    var currentdate = new Date(); 
+    var datetime = currentdate.getDate() + "/"
+    + (currentdate.getMonth()+1)  + "/" 
+    + currentdate.getFullYear() + " @ "  
+    + ('0' + currentdate.getHours()).slice(-2) + ":"  
+    + ('0' + currentdate.getMinutes()).slice(-2) + ":" 
+    + ('0' + currentdate.getSeconds()).slice(-2);
+    document.querySelector('.data').innerHTML = datetime;
+},1000);
