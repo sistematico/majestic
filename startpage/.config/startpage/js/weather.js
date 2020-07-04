0f1ffc24d3f2850ca05a1f0da5f6c727b0ed97fd
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-04 08:21:01 -0400

    Autocommit por GitHub.sh: alterações: add 'startpage/.config/startpage/index.html' add 'startpage/.config/startpage/js/weather.js'

--- startpage/.config/startpage/js/weather.js
+++ startpage/.config/startpage/js/weather.js
@@ -1,5 +1,5 @@
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Campo+Grande&units=metric&appid=3dedab9a1e2b6e1cb120ae2f421b477d').then(function(response) {
     return response.json();
 }).then(function(json) {
-    document.querySelector('.clima').innerHTML = json.main.temp + '°C';
+    document.querySelector('.clima').innerHTML = '<i class="fas fa-thermometer-half"></i> ' + json.main.temp + '°C';
 });
