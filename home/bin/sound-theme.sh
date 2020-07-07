8416ef74b34967c72072f53910206c385374e54e
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-07 08:14:02 -0400

    Autocommit por GitHub.sh: alterações: add 'home/bin/sound-theme.sh'

--- home/bin/sound-theme.sh
+++ home/bin/sound-theme.sh
@@ -6,7 +6,7 @@
 # Autor: Lucas Saliés Brum a.k.a. sistematico <lucas@archlinux.com.br>          #
 #                                                                               #
 # Criado em: 07-07-2020 07:44:51                                                #
-# Modificado em: 07-07-2020 07:44:58                                            #
+# Modificado em: 07-07-2020 08:13:02                                            #
 #                                                                               #
 # Este trabalho está licenciado com uma Licença Creative Commons                #
 # Atribuição 4.0 Internacional                                                  #
@@ -18,15 +18,7 @@ command -v dialog >/dev/null 2>&1 || { echo "Programa dialog não encontrado. Ab
 pacman -Qq libcanberra 1> /dev/null 2> /dev/null || { echo "Pacote libcanberra não encontrado. Abortando..." ; exit 1; }
 pacman -Qq libcanberra-pulse 1> /dev/null 2> /dev/null || { echo "Pacote libcanberra-pulse não encontrado. Abortando..." ; exit 1; }
 
-
-
-
-#xfconf-query -c xsettings -p /Net/EnableEventSounds -s true
-#xfconf-query -c xsettings -p /Net/EnableInputFeedbackSounds -s true
-#xfconf-query -c xsettings -p /Net/SoundThemeName -s "Borealis"
-
 old=$(pwd)
-
 cd /usr/share/sounds
 
 i=1
