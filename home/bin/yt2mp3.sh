53f04a4c1032c8d3a15f8c17b17b01cc7017b9f1
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-07 08:41:01 -0400

    Autocommit por GitHub.sh: alterações: add 'home/bin/yt2mp3.sh'

--- home/bin/yt2mp3.sh
+++ home/bin/yt2mp3.sh
@@ -45,7 +45,7 @@ if [[ ! ${url} =~ $padrao ]]; then
     exit
 else
     #titulo="$(curl "$url" -so - | grep -iPo '(?<=<title>)(.*)(?=</title>)' | sed 's/[^[:alnum:]]\+/ /g' | head -n1)"
-    titulo="$(youtube-dl -s -f mp4 -o '%(id)s.%(ext)s' --print-json --no-warnings "$url" | jq -r '.title')"
+    titulo="$(youtube-dl -s -f mp4 -o '%(id)s.%(ext)s' --print-json --no-warnings ${url} | jq -r '.title')"
 
     if [ ${#titulo} -gt 250 ]; then
         diff=$((${#titulo}-250))
