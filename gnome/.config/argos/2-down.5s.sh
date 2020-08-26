ad89ef35893fc2cedb226b3969da10334f3c29ee
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-08-26 13:47:01 -0400

    Autocommit por GitHub: alterações: add 'gnome/.config/argos/2-down.5s.sh'

--- gnome/.config/argos/2-down.5s.sh
+++ gnome/.config/argos/2-down.5s.sh
@@ -11,4 +11,5 @@ fi
 
 echo "---"
 echo "🎉️ Download | terminal=false bash='${HOME}/bin/videodown.sh'"
+echo "👾️ Video Down | bash='gedit $HOME/bin/videodown.sh' terminal=false"
 echo "✍️ Aria2 | bash='gedit $HOME/.aria2/aria2.conf' terminal=false"
