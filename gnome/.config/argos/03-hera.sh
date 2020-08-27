8aa1034177698ecb68daa9abd9803c53d4493f60
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-08-27 17:00:01 -0400

    Autocommit por GitHub: alterações: add 'gnome/.config/argos/03-hera.sh'

--- gnome/.config/argos/03-hera.sh
+++ gnome/.config/argos/03-hera.sh
@@ -3,5 +3,5 @@
 echo "🤖️"
 echo "---"
 echo "💣️ Reiniciar | terminal=false bash='/home/lucas/.local/bin/restart hera'"
-echo "✍️ Editar | bash='gedit /home/lucas/sshfs/hera/etc/nginx/nginx.conf' terminal=false"
+echo "✍️ Editar | bash='gedit /home/lucas/sshfs/hera/etc/nginx/sites/*.conf' terminal=false"
 
