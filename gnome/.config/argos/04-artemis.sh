0f44f34ddc6a26f508df20a550ec1c6a54866257
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-08-27 17:02:01 -0400

    Autocommit por GitHub: alterações: add 'gnome/.config/argos/03-hera.sh' add 'gnome/.config/argos/04-artemis.sh'

--- gnome/.config/argos/04-artemis.sh
+++ gnome/.config/argos/04-artemis.sh
@@ -2,6 +2,7 @@
 
 echo "👾️"
 echo "---"
+echo "💾️ www | bash='nautilus /home/lucas/sshfs/artemis/var/www/' terminal=false"
 echo "💣️ Reiniciar | terminal=false bash='/home/lucas/.local/bin/restart artemis'"
 echo "✍️ Editar | bash='gedit /home/lucas/sshfs/artemis/etc/nginx/nginx.conf' terminal=false"
 
