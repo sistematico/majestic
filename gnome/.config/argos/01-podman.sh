0b141a133847cffd7ca8a5ce386dab2a558cd620
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-08-28 06:33:01 -0400

    Autocommit por GitHub: alterações: add 'gnome/.config/argos/01-podman.sh'

---
+++ gnome/.config/argos/01-podman.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+icone="🌐️"
+
+echo "🌐️"
+echo "---"
+echo "💾️ www | bash='nautilus /var/www/html' terminal=false"
+echo "💣️ Reiniciar | terminal=false bash='sudo systemctl restart nginx php-fpm'"
+echo "✍️ Editar | bash='sudo gedit /etc/nginx/nginx.conf' terminal=false"
+
