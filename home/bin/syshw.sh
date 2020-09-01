7c3220372e507bb122f24442c0be8107fd523824
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-09-01 00:20:02 -0400

    Autocommit por GitHub: alterações: add 'home/bin/syshw.sh'

--- home/bin/syshw.sh
+++ home/bin/syshw.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#
+# Arquivo: syshw.sh
+#
+# Feito por Lucas Saliés Brum a.k.a. sistematico, <lucas@archlinux.com.br>
+#
+# Criado em: 16/03/2018 16:35:20
+# Última alteração: 01/09/2020 00:19:50
 
 clear
 
