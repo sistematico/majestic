958a323ab4e9b9323ffe46846710eecec502936e
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-09-01 00:21:01 -0400

    Autocommit por GitHub: alterações: add 'home/bin/factory-reset.sh' add 'home/bin/senha.sh'

--- home/bin/factory-reset.sh
+++ home/bin/factory-reset.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#
+# Arquivo: factory-reset.sh
+#
+# Feito por Lucas Saliés Brum a.k.a. sistematico, <lucas@archlinux.com.br>
+#
+# Criado em: 16/03/2018 16:35:20
+# Última alteração: 01/09/2020 00:20:39
 
 echo "Packages: $(pacman -Q | wc -l)" > /var/tmp/packages-before.log
 echo "---" >> /var/tmp/packages-before.log
--- home/bin/senha.sh
+++ home/bin/senha.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#
+# Arquivo: syshw.sh
+#
+# Feito por Lucas Saliés Brum a.k.a. sistematico, <lucas@archlinux.com.br>
+#
+# Criado em: 16/03/2018 16:35:20
+# Última alteração: 01/09/2020 00:20:17
 
 chars=32
 senha="$(openssl rand -base64 $chars)"
