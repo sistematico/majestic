d7dba0bea6ade99ccbd26a8548602be5d341c235
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-10-07 17:36:01 -0400

    Autocommit por GitHub: alterações: remove 'docker/docker/confs/mysql/mysql.sql'

--- docker/docker/confs/mysql/mysql.sql
+++
@@ -1,3 +0,0 @@
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'senha';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'senha';
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'senha';
