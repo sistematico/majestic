fcf8e91318854881f4a0331e432426d2c765fa73
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-10-22 17:55:01 -0400

    Autocommit por GitHub: alterações: add 'dracula/.local/share/gedit/gedit-metadata.xml' add 'gnome/.config/argos/07-wallpaper.sh'

--- gnome/.config/argos/07-wallpaper.sh
+++ gnome/.config/argos/07-wallpaper.sh
@@ -2,4 +2,6 @@
 
 echo "🎨️"
 echo "---"
-echo "🌍️ Download | terminal=false bash='${HOME}/bin/unsplash.sh'"
+echo "🌍️ Download | terminal=false bash='${HOME}/bin/unsplash.sh --download'"
+echo "🌍️ Random | terminal=false bash='${HOME}/bin/unsplash.sh --random'"
+echo "🌍️ Clean | terminal=false bash='${HOME}/bin/unsplash.sh --clean'"
