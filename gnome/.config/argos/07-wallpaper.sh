d1136bbbe04e1a21971b93ef71ba929d439774c4
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-10-22 17:53:01 -0400

    Autocommit por GitHub: alterações: add 'dracula/.local/share/gedit/gedit-metadata.xml' add 'gnome/.config/argos/07-wallpaper.sh'

--- gnome/.config/argos/07-wallpaper.sh
+++ gnome/.config/argos/07-wallpaper.sh
@@ -2,6 +2,4 @@
 
 echo "🎨️"
 echo "---"
-echo "🎉️ Download | terminal=false bash='${HOME}/bin/videodown.sh'"
-echo "👾️ Video Down | bash='gedit $HOME/bin/videodown.sh' terminal=false"
-echo "✍️ Aria2 | bash='gedit $HOME/.aria2/aria2.conf' terminal=false"
+echo "🌍️ Download | terminal=false bash='${HOME}/bin/unsplash.sh'"
