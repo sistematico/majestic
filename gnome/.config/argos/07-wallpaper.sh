a817cf39d7400710dbda8e84f63b3bcf1470d6cd
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-10-22 17:48:01 -0400

    Autocommit por GitHub: alterações: add 'gnome/.config/argos/07-wallpaper.sh'

--- gnome/.config/argos/07-wallpaper.sh
+++ gnome/.config/argos/07-wallpaper.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-NUM=$(ps aux | grep youtube-dl | grep -v grep | wc -l)
-DIR=$(dirname "$0")
-
-if [ $NUM -gt 0 ]; then
-	echo "🌍 ("${NUM}")️"
-else
-	echo "🌍️"
-fi
-
+echo "🎨️"
 echo "---"
 echo "🎉️ Download | terminal=false bash='${HOME}/bin/videodown.sh'"
 echo "👾️ Video Down | bash='gedit $HOME/bin/videodown.sh' terminal=false"
