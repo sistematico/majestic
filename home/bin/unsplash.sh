cace058d3eba38e81ab4a17182de84fba79dcdd1
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-10-25 18:26:01 -0400

    Autocommit por GitHub: alterações: add 'home/bin/unsplash.sh'

--- home/bin/unsplash.sh
+++ home/bin/unsplash.sh
@@ -75,8 +75,12 @@ elif [ "$1" == "--download" ]; then
 	then
 		#if WGET "$imgURL"; then
 		curl --max-time 120 --connect-timeout 10 -L -s "https://source.unsplash.com/${x}x${y}/?nature,water" > "$arquivo"
-		setWall "$arquivo"
-		echo "$arquivo" >> $downloaded
+		if [ "$(file -b --mime-type $arquivo)" != "image/jpeg" ]; then
+			rm -f "$arquivo"
+		else
+			setWall "$arquivo"
+			echo "$arquivo" >> $downloaded
+		fi
 	fi
 elif [ "$1" == "--random" ]; then
 	rdmWall
