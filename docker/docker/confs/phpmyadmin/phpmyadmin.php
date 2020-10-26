daf0cadcef6f595df38516dbe7242b56da8b5d60
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-10-26 10:38:01 -0400

    Autocommit por GitHub: alterações: add 'docker/docker/confs/phpmyadmin/phpmyadmin.php'

--- docker/docker/confs/phpmyadmin/phpmyadmin.php
+++ docker/docker/confs/phpmyadmin/phpmyadmin.php
@@ -3,7 +3,7 @@ $i = 0;
 $i++;
 $cfg['Servers'][$i]['DefaultConnectionCollation'] = 'utf8_general_ci';
 $cfg['Servers'][$i]['hide_db'] = '^(information_schema|performance_schema|mysql|sys)$';
-$cfg['Servers'][$i]['ssl'] = true;
-$cfg['Servers'][$i]['ssl_cert'] = '/etc/ssl/nginx/localhost.pem';
-$cfg['Servers'][$i]['ssl_key'] = '/etc/ssl/nginx/localhost-key.pem';
+#$cfg['Servers'][$i]['ssl'] = true;
+#$cfg['Servers'][$i]['ssl_cert'] = '/etc/ssl/nginx/localhost.pem';
+#$cfg['Servers'][$i]['ssl_key'] = '/etc/ssl/nginx/localhost-key.pem';
 ?>
