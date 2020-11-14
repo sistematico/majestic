7eb01b2f5785fc619ccb8ab6ae15339345510cd7
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-14 19:26:01 -0400

    Autocommit por GitHub: alterações: add 'picom/.config/picom/grayscale-shader.glsl'

---
+++ picom/.config/picom/grayscale-shader.glsl
@@ -0,0 +1,13 @@
+uniform float opacity;
+uniform bool invert_color;
+uniform sampler2D tex;
+
+void main() {
+	vec4 c = texture2D(tex, gl_TexCoord[0].xy);
+	float g = (c.r + c.g + c.b) / 3.0;	// EDIT1: Average.
+	c = vec4(vec3(g), c.a);			// EDIT2: Color.
+ 	if (invert_color)
+		c = vec4(vec3(c.a, c.a, c.a) - vec3(c), c.a);
+	c *= opacity;
+	gl_FragColor = c;
+}
