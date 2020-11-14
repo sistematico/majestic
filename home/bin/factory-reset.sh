12a67a3481af7c78764701effe3f22e88a2ef141
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-13 22:33:01 -0400

    Autocommit por GitHub: alterações: add 'home/bin/factory-reset.sh'

--- home/bin/factory-reset.sh
+++ home/bin/factory-reset.sh
@@ -8,6 +8,7 @@
 # Última alteração: 01/09/2020 00:20:39
 
 log=0
+basepkgs="base linux linux-firmware efibootmgr lvm2 intel-ucode btrfs-progs grub dhcpcd nano terminus-font"
 
 if [ $log -eq 1 ]; then
     echo "Packages: $(pacman -Q | wc -l)" > /var/tmp/packages-before.log
@@ -15,23 +16,41 @@ if [ $log -eq 1 ]; then
     pacman -Q >> /var/tmp/packages-before.log
 fi
 
+
+
+read -p "* Deseja instalar uma interface gráfica? [s/N]: " INTERFACE
+if [[ $INTERFACE == *[sS]* ]]; then
+
+while :
+do    
+	clear
+	echo "---------------------------------"
+	echo "	     M A I N - M E N U"
+	echo "---------------------------------"
+	echo "1. i3-gaps"
+	echo "2. gnome"
+	echo "3. xfce"
+	echo "4. sair"
+	echo "---------------------------------"
+	read -r -p "Escolha uma opção [1-4] : " c
+    case $c in
+		1) pause "$(date)";;
+		2) w| less;;
+		3) echo '*** Top 10 Memory eating process:'; ps -auxf | sort -nr -k 4 | head -10; 
+		   echo; echo '*** Top 10 CPU eating process:';ps -auxf | sort -nr -k 3 | head -10; 
+		   echo;  pause;;
+		4) break;;
+		*) Pause "Select between 1 to 5 only"
+	esac
+done
+
+fi
+
 # Mark all as optional
 pacman -D --asdeps $(pacman -Qqe)
 
 # Mark base packages as explicit
-pacman -D --asexplicit base linux linux-firmware efibootmgr lvm2 intel-ucode btrfs-progs grub dhcpcd nano terminus-font
-
-read -p "Deseja : " IDADE
-
-while :
-do
-    read -p "* Deseja instalar uma interface gráfica? [s/N]: " INTERFACE
-    if [  ]; then
-        break
-    fi
-done
-
-pacman -D --asexplicit sudo ntfs-3g
+pacman -D --asexplicit $basepkgs
 
 # Remove all except explicit packages
 # Note: The arguments -Qt list only true orphans. 
