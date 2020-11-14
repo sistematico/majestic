7c29e34c94fa64226c99e3d939a2d5bac45ce0bc
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-14 09:29:01 -0400

    Autocommit por GitHub: alterações: add 'polybar/.config/polybar/scripts/lock.sh'

---
+++ polybar/.config/polybar/scripts/lock.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cor=$(xrdb -query | grep border | head -n1 | awk '{print $2}')
+
+if [ "$1" == "lock" ]; then
+	$HOME/.local/lock/fortune &
+else
+	if pgrep -x "xautolock" > /dev/null
+	then
+		if [ $1 ]; then
+			killall xautolock &
+			notify-send -i ~/.local/share/icons/elementary/screensaver.png "Screen Locker" "Proteção de tela desativada."
+		fi
+		echo "%{F${cor}}%{F-}"
+	else
+		if [ $1 ]; then 
+			xautolock -time 3 -detectsleep -locker $HOME/.local/lock/fortune &
+			notify-send -i ~/.local/share/icons/elementary/screensaver.png "Screen Locker" "Proteção de tela ativada."
+		fi
+		echo ""
+	fi
+fi
