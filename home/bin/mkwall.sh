e27798d1a4b2dd551eb03fbd6509cfbd891ab39f
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-16 13:03:01 -0400

    Autocommit por GitHub: alterações: add 'home/bin/mkwall.sh'

--- home/bin/mkwall.sh
+++ home/bin/mkwall.sh
@@ -4,19 +4,14 @@ dimensions=$(xdpyinfo  | awk '/dimensions:/ {print $2}')
 dominant=$(convert $HOME/gitlab/lnxpcs/distro/arch/arch-linux-n1.png -scale 50x50! -depth 8 +dither -colors 8 -format "%c" histogram:info: | awk 'NR==1{print $3}')
 
 function mkWall() {
-	convert "$1" -gravity center -resize "${dimensions}" -background "$dominant" -extent "${dimensions}" "$2"
+    nome="${1%.*}"
+    ext="${1##*.}"
+    arquivo=""
+    [ ! -f "$arquivo" ] && convert "$1" -gravity center -resize "${dimensions}" -background "$dominant" -extent "${dimensions}" "${arquivo}"
 }
 
 OIFS="$IFS"
 IFS=$'\n'
-#for file in `find . -type f -name "*.csv"`
-#do
-#     echo "file = $file"
-#     diff "$file" "/some/other/path/$file"
-#     read line
-#done
-
-
 
 if [ $1 ]; then
     imgs=( "$@" )
