ac9f57cb480142b975c2a0dbd0d1af9cd43a87b3
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-16 12:32:01 -0400

    Autocommit por GitHub: alterações: add 'home/bin/mkwall.sh'

--- home/bin/mkwall.sh
+++ home/bin/mkwall.sh
@@ -15,3 +15,7 @@ dominant=$(convert $HOME/gitlab/lnxpcs/distro/arch/arch-linux-n1.png -scale 50x5
 #convert "$scr" -scale 10% -scale 1000% -size "${width}x${height}" -gravity south-west	gradient:none-"$gradientcolor" -composite -matte "$icon" -gravity center -composite -matte -gravity center -size ${width}x30 -pointsize $fontsize	-font $font -fill "#EAE4D1" -annotate $textposition "$fortune" "$scr"
 
 convert -size "${dimensions}" -background "#990000" "$HOME/gitlab/lnxpcs/distro/arch/arch-linux-n1.png" -gravity center "/tmp/teste.png"
+
+convert "$HOME/gitlab/lnxpcs/distro/arch/arch-linux-n1.png" -gravity center -resize "${dimensions}>" \
+	-background "$dominant" -extent "${dimensions}" \
+	"/tmp/teste.png"
