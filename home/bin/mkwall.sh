94970341fa727c6393124fa710eecb37b019608b
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-16 12:39:02 -0400

    Autocommit por GitHub: alterações: add 'home/bin/mkwall.sh'

--- home/bin/mkwall.sh
+++ home/bin/mkwall.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
-
-# get gradient dimensions directly from the screenshot
-#read width height <<<$(file $scr | cut -d, -f 2 | tr -d ' ' | tr 'x' ' ')
-#height=$((height / 2))
-
-width=$(identify -format %w $scr)
-height=$(identify -format %h $scr)
 dimensions=$(xdpyinfo  | awk '/dimensions:/ {print $2}')
 dominant=$(convert $HOME/gitlab/lnxpcs/distro/arch/arch-linux-n1.png -scale 50x50! -depth 8 +dither -colors 8 -format "%c" histogram:info: | awk 'NR==1{print $3}')
 
-#convert -size 100x100 xc:#990000 whatever.png
+function mkWall() {
+	convert "$1" -gravity center -resize "${dimensions}" -background "$dominant" -extent "${dimensions}" "$2"
+}
 
-#convert "$scr" -scale 10% -scale 1000% -size "${width}x${height}" -gravity south-west	gradient:none-"$gradientcolor" -composite -matte "$icon" -gravity center -composite -matte -gravity center -size ${width}x30 -pointsize $fontsize	-font $font -fill "#EAE4D1" -annotate $textposition "$fortune" "$scr"
-
-convert -size "${dimensions}" -background "#990000" "$HOME/gitlab/lnxpcs/distro/arch/arch-linux-n1.png" -gravity center "/tmp/teste.png"
-
-convert "$HOME/gitlab/lnxpcs/distro/arch/arch-linux-n1.png" -gravity center -resize "${dimensions}>" \
-	-background "$dominant" -extent "${dimensions}" \
-	"/tmp/teste.png"
+if [ $1 ]; then
+    for var in "$@"
+    do
+        echo "$var"
+    done
+fi
