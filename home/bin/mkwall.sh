f19f0d47562cbeea30f46c40026b20ea5d261f5e
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-16 13:04:01 -0400

    Autocommit por GitHub: alterações: add 'home/bin/mkwall.sh'

--- home/bin/mkwall.sh
+++ home/bin/mkwall.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 dimensions=$(xdpyinfo  | awk '/dimensions:/ {print $2}')
-dominant=$(convert $HOME/gitlab/lnxpcs/distro/arch/arch-linux-n1.png -scale 50x50! -depth 8 +dither -colors 8 -format "%c" histogram:info: | awk 'NR==1{print $3}')
 
 function mkWall() {
-    nome="${1%.*}"
-    ext="${1##*.}"
-    arquivo=""
+    dominant=$(convert "${1}" -scale 50x50! -depth 8 +dither -colors 8 -format "%c" histogram:info: | awk 'NR==1{print $3}')
+    nome="${1%.*}" ; ext="${1##*.}" ; arquivo="${nome}-${dimensions}.${ext}"
     [ ! -f "$arquivo" ] && convert "$1" -gravity center -resize "${dimensions}" -background "$dominant" -extent "${dimensions}" "${arquivo}"
 }
 
@@ -25,7 +23,7 @@ for img in "${imgs[@]}"
 do
     type="$(file -ib "$img" | awk -F'/' '{print $1}' 2> /dev/null)"
     if [ "$type" == "image" ]; then
-        file -b "$img"
+        mkWall "$img"
     fi
 done
 
