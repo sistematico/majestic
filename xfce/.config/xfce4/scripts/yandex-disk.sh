a02c90a7441019afad5bdab75b0156eec60cb48c
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-12-28 18:35:01 -0400

    Autocommit por GitHub: alterações: add 'xfce/.config/xfce4/scripts/yandex-disk.sh'

--- xfce/.config/xfce4/scripts/yandex-disk.sh
+++ xfce/.config/xfce4/scripts/yandex-disk.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-config="cn"
+config="ls"
 cmd=$(yandex-disk -c ~/.config/yandex-disk/${config}.cfg status | awk -F'[()]' 'NR==1{print $2}')
 
 #if pgrep yandex-disk > /dev/null 2> /dev/null; then
