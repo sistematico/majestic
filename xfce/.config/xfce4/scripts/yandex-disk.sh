2bb4d64b0b58cc55b1e9fd5b3284b5ec0964879b
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-12-28 03:54:01 -0400

    Autocommit por GitHub: alterações: add 'xfce/.config/xfce4/scripts/yandex-disk.sh'

--- xfce/.config/xfce4/scripts/yandex-disk.sh
+++ xfce/.config/xfce4/scripts/yandex-disk.sh
@@ -4,4 +4,4 @@ icon="${HOME}/.local/share/icons/newaita/folder-yandex-disk.svg"
 cmd=$(yandex-disk -c ~/.config/yandex-disk/cn.cfg status | awk -F'[()]' 'NR==1{print $2}')
 
 #echo "<img>${icon}</img> ${cmd}"
-echo "${cmd}"
+echo "Yandex: ${cmd}"
