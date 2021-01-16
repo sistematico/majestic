85ac960761a74412568a8f7f3a94a1d6b3f4cdb8
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-01-16 16:56:01 -0400

    Autocommit por GitHub: alterações: add 'polybar/.config/polybar/dracula' add 'polybar/.config/polybar/scripts/g533.sh'

---
+++ polybar/.config/polybar/scripts/g533.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# downloads.sh
+#
+
+cor=$(xrdb -query | grep border | head -n1 | awk '{print $2}')
+
+if [ "$1" == "c" ]; then
+	killall youtube-dl
+elif [ "$1" == "x" ]; then
+	bash -c "$HOME/bin/videodown.sh"
+elif [ "$1" == "i" ]; then
+	bash -c "$HOME/bin/imgdown.sh"
+else 
+	videos=$(ps -A | grep youtube-dl | wc -l)
+	imagens=$(ps -ef | grep imgdown.sh | grep -v grep | wc -l)
+	
+	if [ $videos -gt 0 ] && [ $imagens -gt 0 ]; then
+		echo "%{F${cor}}%{F-} $videos / $imagens"
+	elif [ $videos -gt 0 ] && [ $imagens -eq 0 ]; then
+		echo "%{F${cor}}%{F-} $videos"
+	elif [ $videos -eq 0 ] && [ $imagens -gt 0 ]; then
+		echo "%{F${cor}}%{F-} $imagens"
+	else
+		echo "%{F${cor}}%{F-} 0"
+	fi
+fi
