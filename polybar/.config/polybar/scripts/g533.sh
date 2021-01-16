898855e9ee2b1c19ac222f0c48d7e81ebe1a0429
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-01-16 17:09:02 -0400

    Autocommit por GitHub: alterações: add 'polybar/.config/polybar/scripts/g533.sh'

--- polybar/.config/polybar/scripts/g533.sh
+++ polybar/.config/polybar/scripts/g533.sh
@@ -3,15 +3,13 @@
 # ~/.config/polybar/scripts/g533.sh
 #
 
-cor=$(xrdb -query | grep border | head -n1 | awk '{print $2}')
-
 if ! sudo /usr/local/bin/headsetcontrol -b 2> /dev/null | grep -q Battery; then
     echo ""
 	exit
 fi
 
+cor=$(xrdb -query | grep border | head -n1 | awk '{print $2}')
 batt=$(sudo /usr/local/bin/headsetcontrol -b | grep Battery | awk '{print $2}')
-#batt=${batt%?}
 
 if [ "$batt" == "Chargin" ]; then
 	echo "%{F${cor}}%{F-} Carr."
