1f56872ff368a466d550008876d54c3c3467a754
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-11 15:13:02 -0400

    Autocommit por GitHub: alterações: add 'gnome/.config/argos/02-nginx.sh'

--- gnome/.config/argos/02-nginx.sh
+++ gnome/.config/argos/02-nginx.sh
@@ -7,4 +7,3 @@ echo "---"
 echo "💾️ www | bash='nautilus /var/www/html' terminal=false"
 echo "💣️ Reiniciar | terminal=false bash='sudo systemctl restart nginx php-fpm'"
 echo "✍️ Editar | bash='sudo gedit /etc/nginx/nginx.conf' terminal=false"
-
