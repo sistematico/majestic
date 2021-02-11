94d94660dedde4549eeb50a02cb73a93b4e3343b
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-11 15:14:01 -0400

    Autocommit por GitHub: alterações: add 'gnome/.config/argos/09-wallhaven.sh'

---
+++ gnome/.config/argos/09-wallhaven.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "🎨️"
+echo "---"
+echo "🌍️ Wallhaven Download | terminal=false bash='${HOME}/bin/wallhaven.sh'"
+echo "💫️ Wallhaven Random | terminal=false bash='${HOME}/bin/wallhaven.sh --random'"
+echo "💥️ Wallhaven Remove | terminal=false bash='${HOME}/bin/wallhaven.sh --delete'"
