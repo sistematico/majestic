c44d296853a6204cf1fadfb28bf3873234ad50b4
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-12 05:18:01 -0400

    Autocommit por GitHub: alterações: add 'gnome/bin/applications.sh'

---
+++ gnome/bin/applications.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Open Applications Gnome Menu
+dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:'Main.shellDBusService.ShowApplications();'
