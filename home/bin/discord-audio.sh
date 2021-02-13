fa96bc4dc2f07a0501c79a0523e8c012d896cfa9
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-12 21:57:02 -0400

    Autocommit por GitHub: alterações: add 'home/bin/discord-audio.sh'

--- home/bin/discord-audio.sh
+++ home/bin/discord-audio.sh
@@ -2,3 +2,7 @@
 
 entrada='alsa_input.usb-Logitech_G533_Gaming_Headset-00.mono-fallback'
 saida='alsa_output.usb-Logitech_G533_Gaming_Headset-00.analog-stereo'
+
+pactl load-module module-null-sink sink_name=Virtual01
+pactl load-module module-loopback source=$entrada sink=Virtual01
+pactl load-module module-loopback source=Virtual01.monitor sink=$saida
