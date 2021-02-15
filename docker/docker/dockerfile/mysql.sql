2bd9e87263b70f7524d94db055e2a1182b6a87b5
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-15 05:30:01 -0400

    Autocommit por GitHub: alterações: add 'docker/docker/dockerfile/mysql.sql'

---
+++ docker/docker/dockerfile/mysql.sql
@@ -0,0 +1,3 @@
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'senha';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'senha';
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'senha';
