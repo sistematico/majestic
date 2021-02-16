95221c013bb761a8663139ba33e8c3f55c6ea5e9
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-16 02:36:01 -0400

    Autocommit por GitHub: alterações: remove 'docker/docker/scripts/portainer.sh' remove 'docker/docker/scripts/rancher.sh' add 'docker/docker/compose/portainer/docker-compose.yml' add 'docker/docker/compose/rancher/docker-compose.yml'

--- docker/docker/scripts/portainer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run -d --name portainer --restart=unless-stopped -p 8080:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
--- docker/docker/scripts/rancher.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run -d --restart=unless-stopped -p 8080:80 -p 443:443 rancher/rancher --name=rancher
