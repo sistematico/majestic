0f9b8a30e5f9984f6537e702d779dd64b465748e
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-24 07:40:01 -0400

    Autocommit por GitHub: alterações: add 'gnome/.config/argos/07-wallpaper.sh' add 'gnome/.config/argos/08-batt.5s.sh' add 'gnome/.config/argos/10-artemis-nginx.sh' add 'gnome/.config/argos/10-gaia-nginx.sh' add 'gnome/.config/argos/10-hera-nginx.sh'

---
+++ gnome/.config/argos/10-hera-nginx.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "👾️"
+echo "---"
+echo "⬇️ down | bash='scp -r root@gaia:/etc/nginx/sites/ /tmp/' terminal=false"
+echo "⬇️ up | bash='scp /tmp/sites/* root@gaia:/etc/nginx/sites/' terminal=false"
+echo "💣️ Reiniciar | terminal=false bash='/home/lucas/.local/bin/restart artemis'"
+
+echo "✍️ Editar | bash='gedit /home/lucas/sshfs/artemis/etc/nginx/nginx.conf /home/lucas/sshfs/artemis/etc/nginx/sites-available/*' terminal=false"
+
