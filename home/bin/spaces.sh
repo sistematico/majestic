e21c5d882ea1e3ac2603b6bba5aa20970bebe3b4
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-25 02:40:02 -0400

    Autocommit por GitHub: alterações: add 'home/bin/spaces.sh'

--- home/bin/spaces.sh
+++ home/bin/spaces.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+[ ! -z $1 ] && [ -f $1 ] && path="${1}" || exit 1
+
 OIFS="$IFS"
 IFS=$'\n'
 #for file in `find . -type f -name "*.csv"`  
-for file in $(cat /tmp/dupes.txt)  
+for file in $(cat $path)  
 do
      #echo "file = $file"
-     rm -i "$file"
+     #rm -i "$file"
      read line
 done
 IFS="$OIFS"
