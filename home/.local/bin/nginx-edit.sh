3f233d0dcf7d37dc953253abb8176ac2476fae41
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-24 20:45:01 -0400

    Autocommit por GitHub: alterações: add 'home/.local/bin/nginx-edit.sh'

--- home/.local/bin/nginx-edit.sh
+++ home/.local/bin/nginx-edit.sh
@@ -2,16 +2,13 @@
 
 host="${1:-hera}"
 
-mkdir /tmp/sites/ && 
+[ -d /tmp/sites ] && rm -rf /tmp/sites
+[ ! -d /tmp/sites ] && mkdir /tmp/sites
 
 rsync -avzz root@${host}:/etc/nginx/sites/ /tmp/sites/ --delete && 
-
 cp -r /tmp/sites/ /tmp/sites-$(date +%s ) &&
-
 gedit /tmp/sites/* && nautilus /tmp/sites/ &&
-
 rsync -avzz /tmp/sites/ root@${host}:/etc/nginx/sites/ --delete &&
-
 ssh root@${host} "systemctl restart nginx" &&
 
 exit
