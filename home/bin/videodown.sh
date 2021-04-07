fcb70877ba8ddff55e4bc8f44e23d69dc09a5c69
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-04-07 08:04:01 -0400

    Autocommit por GitHub: alterações: add 'home/bin/videodown.sh'

--- home/bin/videodown.sh
+++ home/bin/videodown.sh
@@ -29,7 +29,7 @@ TMP="/tmp/videodown/$$"
 LOGS="${DIR}/status.log"
 PROC=$(pgrep -fc "bash $0")
 HEADER="Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"
-NOTIFY="$HOME/bin/notify.sh ${NOME_CURTO} ${ICONE} ${NOME}" # notify-send -h int:transient:1 -i $ICONE
+NOTIFY=$HOME/bin/notify.sh ${NOME_CURTO} ${ICONE} "${NOME}" # notify-send -h int:transient:1 -i $ICONE
 YOUTUBE="youtube-dl -i -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4" # "youtube-dl -i -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4"
 
 [ ! -d "$DIR" ] && mkdir -p $DIR
