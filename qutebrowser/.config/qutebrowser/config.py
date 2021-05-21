31b5dca82b379cc26377a21e4eae1185eceaa317
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-05-21 19:06:01 -0400

    Autocommit por GitHub: alterações: add 'qutebrowser/.config/qutebrowser/config.py'

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -17,7 +17,8 @@ def read_xresources(prefix):
 
 xresources = read_xresources('*')
 
-startpage = 'file:///home/lucas/github/startpages/bento/index.html'
+#startpage = 'file:///home/lucas/github/startpages/bento/index.html'
+startpage = 'https://startpages.lucasbrum.net'
 
 ## This is here so configs done via the GUI are still loaded.
 ## Remove it to not load settings done via the GUI.
