42d78e9c4fc5bedb49ee3341c3fea7ba32bc41b5
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-05-21 14:12:27 -0400

    Autocommit por GitHub: alterações: add 'docker/docker/compose/lemp/docker-compose.yml' remove 'docker/docker/compose/nginx-standalone/docker-compose.yml' remove 'docker/docker/compose/nginx/docker-compose.yml' remove 'docker/docker/confs/nginx/nginx-full.conf' remove 'docker/docker/confs/nginx/nginx-standalone.conf' remove 'docker/docker/confs/nginx/nginx-standalone.old.conf' add 'docker/docker/confs/nginx/nginx.conf' add 'docker/docker/compose/lep/docker-compose.yml'

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -3,7 +3,7 @@
 killall -q polybar
 
 # Dracula
-polybar dracula >> /tmp/polybar-dracula.log 2>&1 & disown
+polybar --config=$HOME/.config/polybar/dracula dracula >> /tmp/polybar-dracula.log 2>&1 & disown
 
 # Transparent
 #polybar top >> /tmp/polybar-top.log 2>&1 & disown
