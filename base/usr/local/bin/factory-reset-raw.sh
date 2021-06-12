50a5191b20a95eaec47a1d844bec1ea0ca247f32
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-06-12 11:21:01 -0400

    Autocommit por GitHub: alterações: add 'base/usr/local/bin/factory-reset-raw.sh'

--- base/usr/local/bin/factory-reset-raw.sh
+++ base/usr/local/bin/factory-reset-raw.sh
@@ -7,7 +7,11 @@
 # Criado em: 16/03/2018 16:35:20
 # Última alteração: 01/09/2020 00:20:39
 
-BASE="base base-devel linux linux-firmware efibootmgr lvm2 intel-ucode btrfs-progs grub dhcpcd sudo nano git terminus-font xorg-server xorg-xinit nvidia"
+BASE="base base-devel linux linux-firmware efibootmgr lvm2 intel-ucode btrfs-progs grub dhcpcd sudo nano openssh git cronie terminus-font xorg-server xorg-xinit nvidia i3-gaps-rounded-git polybar-git feh dunst picom-ibhagwan-git rofi rofi-greenclip ttf-fantasque-sans-mono headsetcontrol-git imagemagick maim fortune-mod fortune-mod-chucknorris"
+
+systemctl --now disable docker
+systemctl disable gdm lightdm
+systemctl enable systemd-networkd
 
 echo "Packages: $(pacman -Q | wc -l)" > /var/tmp/packages-before.log
 echo "---" >> /var/tmp/packages-before.log
