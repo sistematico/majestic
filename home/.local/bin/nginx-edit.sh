ea57b30f8590bb660bfbad1163fe05029aba7b25
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-04 13:30:01 -0400

    Autocommit por GitHub: alterações: add 'home/.local/bin/nginx-edit.sh'

--- home/.local/bin/nginx-edit.sh
+++ home/.local/bin/nginx-edit.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 host="${1:-hera}"
+editor="mousepad"
+filemanager="thunar"
 
 [ -d /tmp/sites ] && rm -rf /tmp/sites
 [ ! -d /tmp/sites ] && mkdir /tmp/sites
@@ -10,7 +12,7 @@ rsync -avzz root@${host}:/etc/nginx/sites/ /tmp/sites/ --delete &&
 rsync -avzz root@${host}:/etc/nginx/snippets/ /tmp/nginx-snippets/ --delete &&
 cp -r /tmp/sites/ /tmp/sites-$(date +%s ) &&
 cp -r /tmp/nginx-snippets/ /tmp/nginx-snippets-$(date +%s ) &&
-gedit /tmp/sites/* && nautilus /tmp/sites/ /tmp/nginx-snippets/ &&
+$editor /tmp/sites/* && $filemanager /tmp/sites/ /tmp/nginx-snippets/ &&
 rsync -avzz /tmp/sites/ root@${host}:/etc/nginx/sites/ --delete &&
 rsync -avzz /tmp/nginx-snippets/ root@${host}:/etc/nginx/snippets/ --delete &&
 ssh root@${host} "systemctl restart nginx" &&
