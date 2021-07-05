47226a049bd4f94cf675f1bbf5d702a1a931d4d4
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-05 18:32:01 -0400

    Autocommit por GitHub: alterações: add 'home/.local/bin/docker-edit.sh'

--- home/.local/bin/docker-edit.sh
+++ home/.local/bin/docker-edit.sh
@@ -1,27 +1,8 @@
 #!/usr/bin/env bash
 
-host="${1:-hera}"
-
-editor="mousepad"
+editor="code"
 filemanager="thunar"
+confs="$HOME/docker/confs"
 
-nginx_tmp="/tmp/nginx"
-snippets_tmp="/tmp/snippets"
-
-[ ! -d $nginx_tmp ] && mkdir $nginx_tmp
-[ ! -d $snippets_tmp ] && mkdir $snippets_tmp
-
-rsync -avzz root@${host}:/etc/nginx/sites/ $nginx_tmp/ --delete &&
-rsync -avzz root@${host}:/etc/nginx/snippets/ $snippets_tmp/ --delete &&
-
-cp -a $nginx_tmp $nginx_tmp-$(date +%s) &&
-cp -a $snippets_tmp $snippets_tmp-$(date +%s) &&
-
-$editor $nginx_tmp/* && $filemanager $nginx_tmp $snippets_tmp &&
-
-rsync -avzz $nginx_tmp/ root@${host}:/etc/nginx/sites/ --delete &&
-rsync -avzz $snippets_tmp/ root@${host}:/etc/nginx/snippets/ --delete &&
-
-ssh root@${host} "systemctl restart nginx" &&
-
-exit
+$editor $confs/nginx.conf && $filemanager $confs &&
+docker restart nginx && exit
