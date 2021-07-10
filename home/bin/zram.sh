edeff2b3e30ade3b8187869f1d2d032c2d1ef42e
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-10 14:46:02 -0400

    Autocommit por GitHub: alterações: add 'home/bin/zram.sh'

---
+++ home/bin/zram.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ ! $1 ]; then
+    echo "Usage: $(basename $0) [enable|disable]"
+    exit
+fi
+
+if [ "$1" == "enable" ]; then
+    modprobe zram
+    echo lz4 > /sys/block/zram0/comp_algorithm
+    echo 32G > /sys/block/zram0/disksize
+    mkswap --label zram0 /dev/zram0
+    swapon --priority 100 /dev/zram0
+fi
+
+if [ "$1" == "disable" ]; then
+    swapoff /dev/zram0
+    rmmod zram
+fi
