deba9a633218c4f555043d08fc161e28cba8fead
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-08-08 11:15:43 -0400

    Updates

--- docker/docker/run.sh
+++ docker/docker/run.sh
@@ -6,12 +6,43 @@ TEMP_PATH="/tmp/docker"
 PROJECT_PATH="$HOME/github"
 PROJECT_NAME="laravel"
 
+function progress() {
+    pid=$!; spin='-\|/'; i=0
+
+    reset='\e[0m'; black=30; red=31; green=32; yellow=33; blue=34; magenta=35; cyan=36; white=37
+
+    case $2 in
+        hello)
+            echo "Hello yourself!"
+            ;;
+        bye)
+            echo "See you again!"
+            break
+            ;;
+        *)
+            cor='\e[0m'
+            ;;
+    esac
+
+
+    while kill -0 $pid 2>/dev/null
+    do
+        i=$(( (i+1) %4 ))
+        # echo -ne "\r\e[1;31m[\e[1;32m${spin:$i:1}\e[1;31m] \e[1;33m$1 \e[0m"
+        #printf "\r [${spin:$i:1}] $1"
+        printf "\r\e[1;31m[\e[1;32m ${spin:$i:1} \e[1;31m] \e[1;33m$1 \e[0m"
+        sleep .1
+    done
+}
+
 [ ! -f /var/run/docker.sock ] && sudo systemctl start docker
 [ ! -d $PROJECT_PATH ] && mkdir $PROJECT_PATH
 [ -d $DOCKER_PATH ] && mv $DOCKER_PATH ~/.local/share/Trash/files/docker-$(date +%s)
 [ ! -d $TEMP_PATH ] && git clone https://github.com/sistematico/majestic $TEMP_PATH
 
-cd $TEMP_PATH && git pull
+cd $TEMP_PATH && git pull > /dev/null &
+progress "Efetuando o pull"
+
 cp -a $TEMP_PATH/docker/docker $DOCKER_PATH
 
 mkdir $DOCKER_PATH/certs/
@@ -35,17 +66,28 @@ fi
 
 [ -d $PROJECT_PATH/$PROJECT_NAME ] && mv $PROJECT_PATH/$PROJECT_NAME ~/.local/share/Trash/files/laravel-$(date +%s)
 
-composer create-project laravel/laravel --prefer-dist $PROJECT_PATH/$PROJECT_NAME 2> /dev/null > /dev/null
+composer create-project laravel/laravel --prefer-dist $PROJECT_PATH/$PROJECT_NAME 2> /dev/null > /dev/null &
+progress "Criando o projeto..."
 
 if grep -Fxq "$PROJECT_NAME" /etc/hosts 2> /dev/null;
 then
     sudo sed -i.bak "s|127.0.0.1|$ $PROJECT_NAME|g" /etc/hosts
 fi
 
-docker stop laravel-nginx laravel-php laravel-memcached laravel-redis laravel-mailhog laravel-mysql laravel-phpmyadmin laravel-mix > /dev/null 2> /dev/null
-docker rm laravel-nginx laravel-php laravel-memcached laravel-redis laravel-mailhog laravel-mysql laravel-phpmyadmin laravel-mix > /dev/null 2> /dev/null
+# docker stop laravel-nginx laravel-php laravel-memcached laravel-redis laravel-mailhog laravel-mysql laravel-phpmyadmin laravel-mix 1> /dev/null 2> /dev/null &
+docker stop $(docker ps -a -q) 2> /dev/null > /dev/null &
+progress "Parando containers..."
+
+#docker rm laravel-nginx laravel-php laravel-memcached laravel-redis laravel-mailhog laravel-mysql laravel-phpmyadmin laravel-mix 1> /dev/null 2> /dev/null &
+docker rm $(docker ps -a -q) 2> /dev/null > /dev/null &
+progress "Removendo containers..."
 
 cd $DOCKER_PATH/compose/laravel 
-docker-compose up -d --build > /dev/null 2> /dev/null
+
+#docker-compose up -d --build > /dev/null 2> /dev/null &
+docker-compose up -d --build
+progress "Configurando o Docker..."
+
+echo
 
 google-chrome-stable https://laravel
