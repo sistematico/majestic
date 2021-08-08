5a0a0ece69de4bb34432eb9d15da6cde7994d6dd
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-08-08 09:00:18 -0400

    Updates

--- docker/docker/run.sh
+++ docker/docker/run.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 
-[ -d /tmp/majestic ] && rm -rf /tmp/majestic
-
 [ -d ~/newdocker ] && rm -rf ~/newdocker
 
-git clone https://github.com/sistematico/majestic /tmp/majestic
+git clone https://github.com/sistematico/majestic ~/newdocker
+mkdir ~/newdocker/certs/
 
-cp -a /tmp/majestic/docker/docker ~/newdocker
+if ! command -v mkcert &> /dev/null
+then
+    sudo curl -s -L https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64 > /usr/local/bin/mkcert
+    exit
+fi
 
-cd ~/newdocker
+if [ ! -f cd ~/newdocker/certs/laravel.pem ]; then
+    cd ~/newdocker/certs/
+    mkcert -install
+    mkcert laravel
+fi
+
+cd ~/newdocker/compose/laravel
+
+if ! grep -Fxq "laravel" /etc/hosts
+then
+    sudo sed '/127.0.0.1/s/$/ laravel/' /etc/hosts
+fi
+
+docker stop laravel-nginx laravel-php laravel-memcached laravel-redis laravel-mailhog laravel-mysql laravel-phpmyadmin laravel-mix
+docker rm laravel-nginx laravel-php laravel-memcached laravel-redis laravel-mailhog laravel-mysql laravel-phpmyadmin laravel-mix
+
+docker-compose up -d --build
+
+google-chrome-stable https://laravel
