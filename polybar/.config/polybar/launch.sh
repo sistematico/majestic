43c2f8ae497825e2bb0280ca55b8bf34f28a8c6f
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-10-22 17:37:40 -0400

    Autocommit por GitHub: alterações: add 'dunst/.config/dunst/dracula' add 'dunst/.config/dunst/dunstrc' add 'home/.Xresources' add 'home/.xprofile' add 'i3/.colors/dracula' add 'polybar/.config/polybar/config' add 'polybar/.config/polybar/dracula' add 'polybar/.config/polybar/launch.sh' add 'rofi/.config/rofi/config.rasi' add 'rofi/.config/rofi/scripts/apps' add 'rofi/.config/rofi/scripts/configs' add 'rofi/.config/rofi/scripts/run' add 'vscode/.config/Code/User/settings.json' add 'vscodium/.config/Code/User/settings.json' add 'rofi/.config/rofi/config.old.rasi' add 'rofi/.config/rofi/themes/dracula.rasi' add 'rofi/.config/rofi/themes/dracula2.rasi'
    Signed-off-by: Lucas Saliés Brum <lucas@archlinux.com.br>

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,15 +2,6 @@
 
 killall -q polybar
 
-polybar --config=$HOME/.config/polybar/config gruvbox >> /tmp/polybar.log 2>&1 & disown
-
-# Transparent
-#polybar top >> /tmp/polybar-top.log 2>&1 & disown
-#ln -s /tmp/polybar_mqueue.$! /tmp/ipc-top
-
-#polybar bottom >> /tmp/polybar-bottom.log 2>&1 & disown
-#ln -s /tmp/polybar_mqueue.$! /tmp/ipc-bottom
-
-#echo hook:module/netx >>/tmp/polybar_mqueue.*
+polybar --config=$HOME/.config/polybar/config dracula >> /tmp/polybar.log 2>&1 & disown
 
 
