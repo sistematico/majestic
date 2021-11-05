50cd9d8d9654b50e0b50ae6525c3a688c1c2508f
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-11-05 13:46:55 -0400

    Autocommit por GitHub: alterações: add 'home/.local/bin/screencast' add 'home/bin/videodown-semifull.sh' add 'home/.local/bin/screencast.old' add 'home/bin/videodown-simple.sh' add 'vscodium/.config/Code/User/snippets/bootstrap.json'
    Signed-off-by: Lucas Saliés Brum <lucas@archlinux.com.br>

--- home/bin/videodown-simple.sh
+++ home/bin/videodown-simple.sh
@@ -20,14 +20,10 @@ NOME="Video Down"
 NOME_CURTO="VideoDown"
 SECONDS=0
 COMECO=$SECONDS
-LOG=0 # 0 = Sem log, 1 = Log no arquivo
-ARIA=1
 TS=$(date +"%s")
 DIR="${XDG_DESKTOP_DIR:-${HOME}/desk}"
 ICONE="/usr/share/icons/Newaita/devices/64/video-display.svg"
-TMP="/tmp/videodown/$$"
 LOGS="${DIR}/status.log"
-PROC=$(pgrep -fc "bash $0")
 HEADER="Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"
 NOTIFY="$HOME/bin/notify.sh $NOME_CURTO $ICONE $NOME_CURTO" # notify-send -h int:transient:1 -i $ICONE
 YOUTUBE="youtube-dl" # "youtube-dl -i -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4"
@@ -58,10 +54,7 @@ checkUrl() {
 }
 
 [ ! -d "$DIR" ] && mkdir -p $DIR
-[ ! -d $TMP ] && mkdir -p $TMP
 [ $1 ] && url="$1" || url="$(xclip -o)"
-
-$NOTIFY "URL: <b>$url</b>"
 
 cd $DIR
 
@@ -76,56 +69,15 @@ then
     exit 1
 fi
 
-if grep -Fxq "$url" $HISTORY
-then
-    $NOTIFY "O vídeo já foi baixado!"
-    exit 1
-fi
-
-titulo="$(curl -A "$HEADER" "$url" -Lso - | grep -iPo '(?<=<title>)(.*)(?=</title>)' | sed 's/[^[:alnum:]]\+/ /g' | head -n1)"
-
-if [ -z "$titulo" ]; then
-    $NOTIFY "Link inválido."
-    exit 1
-fi
-
-if [ ${#titulo} -gt 250 ]; then
-	diff=$((${#titulo}-250))
-	trim=$((${#titulo}-$diff))
-	titulo=${titulo::-$trim}
-fi
-
-if [[ $LOG -ne 0 ]]; then
-    echo "---------------------------------------------------------------" >> "$LOGS"
-    echo "Status:       INICIO" >> "$LOGS"
-    echo "Título:       $titulo" >> "$LOGS"
-    echo "URL:          $url" >>"$LOGS"
-    echo "Path:         $DIR" >> "$LOGS"
-    echo "Temp:         $TMP" >> "$LOGS"
-    echo "Processos:    $PROC" >> "$LOGS"
-fi
-
-$NOTIFY "Início: <b>$titulo</b>"
-
-if [ $ARIA == 1 ]; then
-    $YOUTUBE -o "${titulo}.mp4" --external-downloader aria2c "${url}" # "%(title).%(ext)s"
-    status=$?
-else
-    $YOUTUBE -o "${titulo}.mp4" "${url}"
-    status=$?
-fi
+$YOUTUBE "${url}"
 
 if [[ $status -ne 0 ]]; then
     echo "---------------------------------------------------------------" >> "$LOGS"
     echo "Status:       ERRO" >> "$LOGS"
-    echo "Título:       $titulo" >> "$LOGS"
     echo "URL:          $url" >>"$LOGS"
-    echo "Path:         $DIR" >> "$LOGS"
-	$NOTIFY "Erro: <b>$titulo</b>"
-    exit
+	$NOTIFY "Erro"
+    exit 1
 fi
-
-[[ $status -eq 0 ]] && echo $url >> $HISTORY
 
 final=$SECONDS
 diff=$((final - COMECO))
@@ -146,19 +98,5 @@ else
     tamanho="${tamanho} KB"
 fi
 
-if [[ $LOG -ne 0 ]]; then
-    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> "$LOGS"
-    echo "Status:       SUCESSO" >> "$LOGS"
-    echo "Título:       $titulo" >> "$LOGS"
-    echo "URL:          $url" >>"$LOGS"
-    echo "Path:         $DIR" >> "$LOGS"
-    echo "Temp:         $TMP" >> "$LOGS"
-    echo "Processos:    $PROC" >> "$LOGS"
-    echo >> "$LOGS"
-    echo "Tempo decorrido: ${hora}:${minuto}:${segundo}" >> "$LOGS"
-    echo "Tamanho do arquivo: ${tamanho}\nVelocidade média: ${tempo}KBps" >> "$LOGS"
-    echo "Velocidade média: ${tempo}KBps" >> "$LOGS"
-fi
-
-$NOTIFY "Tempo decorrido: ${hora}:${minuto}:${segundo}\nTamanho do arquivo: ${tamanho}\nVelocidade média: ${tempo}KBps\n\nSucesso: <b>$titulo</b>"
+$NOTIFY "Sucesso!\n\nTempo decorrido: ${hora}:${minuto}:${segundo}"
 exit 0
