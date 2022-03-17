2142c0d49307a4c3a52486a5673cd3d486bb04e7
Lucas Saliés Brum <lucas@archlinux.com.br>
2022-03-17 18:17:19 -0400

    Autocommit por GitHub: alterações: add 'home/.local/bin/disable-iptables.sh'

---
+++ home/.local/bin/disable-iptables.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -F
+sudo iptables -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+sudo iptables -t mangle -F
+sudo iptables -t mangle -X
