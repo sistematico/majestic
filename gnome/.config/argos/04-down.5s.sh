b798e6fc307db753e84e61e0587826319b5de539
Lucas Saliés Brum <97509+sistematico@users.noreply.github.com>
2023-01-08 22:32:54 -0400

    Alterações:  M gnome/.config/argos/04-down.5s.sh

--- gnome/.config/argos/04-down.5s.sh
+++ gnome/.config/argos/04-down.5s.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-NUM=$(ps aux | grep yt-dlp | grep -v grep | wc -l)
+I=$(ps aux | grep yt-dlp | grep -v grep | wc -l)
+Q=$(cat $HOME/.vdown/queue.txt | wc -l)
 DIR=$(dirname "$0")
 
-if [ $NUM -gt 0 ]; then
-	echo "🌍 ("${NUM}")️"
+if [ $I -gt 0 ] || [ $Q -gt 0 ]; then
+	echo "🌍 (i: "${I}" q: "${Q}")️"
 else
 	echo "🌍️"
 fi
 
 echo "---"
 echo "🎉️ Download | terminal=false bash='${HOME}/.dwm/scripts/vdown'"
-echo "👾️ Video Down | bash='gedit ${HOME}/.dwm/scripts/vdown' terminal=false"
+echo "👾️ Video Down | bash='code ${HOME}/.dwm/scripts/vdown' terminal=false"
 echo "✍️ Aria2 | bash='gedit $HOME/.config/aria2/daemon.conf' terminal=false"
 echo "❌ Matar Processos | bash='pkill yt-dlp && pkill aria2c' terminal=false"
 echo "🧾️ Ver Logs | bash='nautilus ~/.vdown/logs/' terminal=false"
